e3cc68479df6d7d8b94a380f725d58a5670510f0
ewi josepha <ewijosepha@gmail.com>
2023-09-02 12:47:14 +0200

    adding a readme.md file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,49 +1,47 @@
-  // const image=document.querySelector('imagecarrier')
+// const image=document.querySelector('imagecarrier')
 
-let imag = document.getElementById("imgs")
-let button1 = document.getElementById("left")
-let button2 = document.getElementById("right")
+const imag = document.getElementById('imgs')
+const button1 = document.getElementById('left')
+const button2 = document.getElementById('right')
   
 // let container=document.querySelector('container')
+const immg = document.querySelectorAll('#imgs img')
 
-let immg = document.querySelectorAll("#imgs img")
-
-  // console.log(immg)
-  
-let indexxx = 0;
+// console.log(immg)
+let indexxx = 0
 let interval = setInterval(run, 2000)
 
-function run() {
-  indexxx++;
-  changee();
+function run () {
+  indexxx++
+  changee()
 }
 
-function changee() {
+function changee () {
   if (indexxx > immg.length - 1) {
-    indexxx = 0;
+    indexxx = 0
   } else if (indexxx < 0) {
-    indexxx = immg.length - 1;
-    console.log(changee);
+    indexxx = immg.length - 1
+    console.log(changee)
   }
 
   imag.style.transform = `translateX(${-indexxx * 500}px)`;
 }
 
-function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 2000);
+function resetInterval () {
+  clearInterval(interval)
+  interval = setInterval(run, 2000)
 }
 
-button1.addEventListener("click", () => {
-  indexxx++;
+button1.addEventListener('click', () => {
+  indexxx++
 
-  changee();
-  resetInterval();
-});
+  changee()
+  resetInterval()
+})
 
-button2.addEventListener("click", () => {
-  indexxx--;
+button2.addEventListener('click', () => {
+  indexxx--
 
-  changee();
-  resetInterval();
-});
+  changee()
+  resetInterval()
+})
