4e9e4a0a90e5f8937e0c60507ed6533c2307b56d
ewi josepha <ewijosepha@gmail.com>
2024-02-23 20:16:46 +0100

    changing method for displaying images

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,44 +3,55 @@
 const imag = document.getElementById('imgs')
 const button1 = document.getElementById('left')
 const button2 = document.getElementById('right')
-// let container=document.querySelector('container')
+// let container=document.querySel xsector('container')
 const immg = document.querySelectorAll('#imgs img')
 
+
+
+let immgLenght = immg.length
+let indexx = 0
+while(indexx < immgLenght ){
+
+console.log(indexx);
+  indexx++
+}
+
+
 // console.log(immg)
-let indexxx = 0
-let interval = setInterval(run, 2000)
+// let indexxx = 0
+// let interval = setInterval(run, 2000)
 
-function run () {
-  indexxx++
-  changee()
-}
+// function run () {
+//   indexxx++
+//   changee()
+// }
 
-function changee () {
-  if (indexxx > immg.length - 1) {
-    indexxx = 0
-  } else if (indexxx < 0) {
-    indexxx = immg.length - 1
-    console.log(changee)
-  }
+// function changee () {
+//   if (indexxx > immg.length - 1) {
+//     indexxx = 0
+//   } else if (indexxx < 0) {
+//     indexxx = immg.length - 1
+//     console.log(changee)
+//   }
 
-  imag.style.transform = `translateX(${-indexxx * 500}px)`
-}
+//   imag.style.transform = `translateX(${-indexxx * 500}px)`
+// }
 
-function resetInterval () {
-  clearInterval(interval)
-  interval = setInterval(run, 2000)
-}
+// function resetInterval () {
+//   clearInterval(interval)
+//   interval = setInterval(run, 2000)
+// }
 
-button1.addEventListener('click', () => {
-  indexxx++
+// button1.addEventListener('click', () => {
+//   indexxx++
 
-  changee()
-  resetInterval()
-})
+//   changee()
+//   resetInterval()
+// })
 
-button2.addEventListener('click', () => {
-  indexxx--
+// button2.addEventListener('click', () => {
+//   indexxx--
 
-  changee()
-  resetInterval()
-})
+//   changee()
+//   resetInterval()
+// })
