2fdee0b7cc6d5d51ae6576d697ed8f005ff6f3f2
ewi josepha <ewijosepha@gmail.com>
2024-02-23 20:38:24 +0100

    changing method for displaying images

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,7 @@
-// const image=document.querySelector('imagecarrier')
-
 const imag = document.getElementById('imgs')
 const button1 = document.getElementById('left')
 const button2 = document.getElementById('right')
-// let container=document.querySel xsector('container')
 const immg = document.querySelectorAll('#imgs img')
-
-
-
-// let immgLenght = immg.length
-// let indexx = 0
-// while(indexx < immgLenght ){
-// imag.style.transform = `translateX(${-indexx * 500}px)`
-// console.log(indexx);
-//   indexx++
-// }
-
 
 let indexxx = 0
 let interval = setInterval(() => {
@@ -34,10 +20,7 @@ function changee() {
 
 function resetInterval() {
   clearInterval(interval)
-  interval = setInterval(() => {
-    indexxx++
-    changee()
-  }, 2000)
+  interval
 }
 
 button1.addEventListener('click', () => {
